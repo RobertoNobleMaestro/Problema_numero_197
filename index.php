ab3f6851d8a7021278ae5a7894521d29866db4b7
RobertoNobleMaestro <10007851.joan23@fje.edu>
2024-09-30 16:12:32 +0200

    transformación de x'->x hecha

---
+++ index.php
@@ -0,0 +1,51 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="GET">
+        <label for="mensaje">Escribe el mensaje que quieres desencriptar: </label>
+        <input type="text" id="mensaje" name="mensaje">
+        <link rel="stylesheet" href="./css/style.css">
+        <button type="submit">Enviar</button>
+    </form>
+    <script>
+        
+
+    </script>
+    <?php 
+    if (isset($_GET['mensaje'])) {
+        $vocales = array('a', 'e', 'i', 'o', 'u');
+        $mensaje = $_GET['mensaje'];    
+        $letras_array = str_split($mensaje);
+        $resultado = [];
+        $consonantes = '';
+        foreach ($letras_array as $letra) {
+            if (in_array(strtolower($letra), $vocales)) {
+                // Invertir consonantes acumuladas si son más de una
+                if (strlen($consonantes) > 1) {
+                    $resultado[] = strrev($consonantes);
+                } else {
+                    $resultado[] = $consonantes;
+                }
+                // Añadir la letra al resultado
+                $resultado[] = $letra;
+                // Limpiar consonantes 
+                $consonantes = '';
+            } else {
+                // Acumular consonantes
+                $consonantes .= $letra;
+            }
+        }
+        // Añadir consonantes restantes al final
+        if (strlen($consonantes) > 1) {
+            $resultado[] = strrev($consonantes);
+        } else {
+            $resultado[] = $consonantes;
+        }
+        // Imprimir el resultado
+        echo implode('', $resultado);
+    }
+    ?>
+</body>
